f2d7d6ed6014ded457f45a1fe02874c82dea6d39
sa46lll <azure394@gmail.com>
2023-06-23 10:19:20 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/kestrel/weddingbookkeeper/WeddingBookKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.kestrel.weddingbookkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingBookKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeddingBookKeeperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kestrel/weddingbookkeeper/WeddingBookKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kestrel.weddingbookkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingBookKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
