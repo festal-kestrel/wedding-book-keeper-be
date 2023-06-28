ad7923977879490a5bde7f4f3d1666dacff47042
sa46lll <azure394@gmail.com>
2023-06-29 04:10:14 +0900

    fix: 클래스명 변경

--- src/main/java/com/kestrel/weddingbookkeeper/api/member/exception/MemberNotFoundException.java
+++ src/main/java/com/kestrel/weddingbookkeeper/api/member/exception/MemberNotFoundException.java
@@ -3,9 +3,9 @@ package com.kestrel.weddingbookkeeper.api.member.exception;
 import com.kestrel.weddingbookkeeper.api.error.exception.BusinessException;
 import org.springframework.http.HttpStatus;
 
-public class MemberNotFountException extends BusinessException {
+public class MemberNotFoundException extends BusinessException {
 
-    public MemberNotFountException() {
+    public MemberNotFoundException() {
         super(HttpStatus.NOT_FOUND, "Member is not found");
     }
 }
