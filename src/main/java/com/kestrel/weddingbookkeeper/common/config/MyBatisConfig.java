0c5c5c685b60c2fcf2336a0cba3c3aab7372b566
sa46lll <azure394@gmail.com>
2023-06-29 02:41:26 +0900

    feat: myBatis JdbcTypeForNull 설정
    issue: #11

---
+++ src/main/java/com/kestrel/weddingbookkeeper/common/config/MyBatisConfig.java
@@ -0,0 +1,16 @@
+package com.kestrel.weddingbookkeeper.common.config;
+
+import org.apache.ibatis.type.JdbcType;
+import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisConfig {
+
+    @Bean
+    public ConfigurationCustomizer mybatisConfigurationCustomizer() {
+        return configuration -> configuration.setJdbcTypeForNull(JdbcType.NULL);
+    }
+}
+
