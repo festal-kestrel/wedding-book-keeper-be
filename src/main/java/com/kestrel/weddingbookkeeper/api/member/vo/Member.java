49388049f703f91a616ce0bc148a25251f125dbd
sa46lll <9812smh@naver.com>
2023-07-02 02:39:34 +0900

    feat: 배우자 인증코드 발급 여부 필드 추가
    issue: #32

--- src/main/java/com/kestrel/weddingbookkeeper/api/member/vo/Member.java
+++ src/main/java/com/kestrel/weddingbookkeeper/api/member/vo/Member.java
@@ -3,9 +3,7 @@ package com.kestrel.weddingbookkeeper.api.member.vo;
 import com.kestrel.weddingbookkeeper.api.date.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @NoArgsConstructor
@@ -16,5 +14,12 @@ public class Member extends BaseEntity {
     private String email;
     private String name;
     private Gender gender;
+    private boolean isPartnerCodeIssued;
 
+    public Member(Integer memberId, String email, String name, Gender gender) {
+        this.memberId = memberId;
+        this.email = email;
+        this.name = name;
+        this.gender = gender;
+    }
 }
