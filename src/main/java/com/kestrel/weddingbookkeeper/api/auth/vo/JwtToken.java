3d30d013902344abc9399fcdba0206a6673062f9
Sangwon <sangcho617@gmail.com>
2023-07-01 18:44:17 +0900

    refactor: 예시를 oauthcontroller에 적용

--- src/main/java/com/kestrel/weddingbookkeeper/api/auth/vo/JwtToken.java
+++ src/main/java/com/kestrel/weddingbookkeeper/api/auth/vo/JwtToken.java
@@ -1,8 +1,11 @@
 package com.kestrel.weddingbookkeeper.api.auth.vo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "jwt 토큰")
 public class JwtToken {
 
-    private long id;
+    @Schema(description = "jwt 토큰 값")
     private String jwtToken;
 
     public JwtToken(String jwtToken) {
--- src/main/java/com/kestrel/weddingbookkeeper/common/configuration/SwaggerConfig.java
+++ src/main/java/com/kestrel/weddingbookkeeper/common/configuration/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.kestrel.weddingbookkeeper.config;
+package com.kestrel.weddingbookkeeper.common.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +17,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.OAS_30)
                 .useDefaultResponseMessages(false)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.kestrel.weddingbookkeeper.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.kestrel.weddingbookkeeper.api"))
                 .paths(PathSelectors.ant("/api/v1/**"))
                 .build()
                 .apiInfo(apiInfo());
