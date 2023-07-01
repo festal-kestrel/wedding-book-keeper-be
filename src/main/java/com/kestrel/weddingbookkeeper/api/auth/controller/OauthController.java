53365c72b662d57d10bee1924bc4ded17c4194ea
Sangwon <40656716+swc617@users.noreply.github.com>
2023-07-01 19:19:15 +0900

    Swagger 세팅 (#25)
    * feat: swagger 초기 세팅
    
    * feat: testcontroller 예시 추가
    
    * refactor: 불필요한 주석 제거
    
    * refactor: 예시를 oauthcontroller에 적용
    
    ---------
    
    Co-authored-by: Sangwon Cho <>

--- src/main/java/com/kestrel/weddingbookkeeper/api/auth/controller/OauthController.java
+++ src/main/java/com/kestrel/weddingbookkeeper/api/auth/controller/OauthController.java
@@ -8,6 +8,14 @@ import com.kestrel.weddingbookkeeper.api.auth.utils.AuthTokenProvider;
 import com.kestrel.weddingbookkeeper.api.auth.utils.KakaoUtil;
 import com.kestrel.weddingbookkeeper.api.member.service.MemberService;
 import com.kestrel.weddingbookkeeper.api.member.vo.Member;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Oauth API", description = "로그인 처리 API")
 @RestController
 @RequestMapping("/api/v1/oauth")
 public class OauthController {
@@ -33,6 +42,14 @@ public class OauthController {
         this.authTokenProvider = authTokenProvider;
     }
 
+    @Operation(summary = "유저 인증", description = "유저의 카카오 토큰으로 해당 유저의 프로필 정보를 저장 하고 jwt 토큰을 발행 후 반환", tags = {"Oauth API"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "ok", content = {@Content(schema = @Schema(implementation = JwtToken.class))}),
+            @ApiResponse(responseCode = "401", description = "unauthorized"),
+    })
+    @Parameters({
+            @Parameter(name="token", description="카카오 api에서 인가를 받은 후 발행된 토큰")
+    })
     @PostMapping("/authorize")
     public ResponseEntity<JwtToken> getMemberInfo(@RequestParam String token) throws JsonProcessingException {
 
---
+++ src/main/java/com/kestrel/weddingbookkeeper/common/configuration/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.kestrel.weddingbookkeeper.common.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.kestrel.weddingbookkeeper.api"))
+                .paths(PathSelectors.ant("/api/v1/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("너에게난,나에게넌 명세서")
+                .description("네어게난,나에게넌 API 명세서")
+                .version("1.0")
+                .build();
+    }
+}
