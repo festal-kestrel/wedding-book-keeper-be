a2f8bae09ef52cf55d1d04b339b502e95e7ca723
서명현 <azure394@gmail.com>
2023-07-01 16:53:24 +0900

    feat: Redis 세팅 (#31)
    * feat: EmbeddedRedis 세팅
    
    issue: #30
    
    * feat: spring-boot-starter-data-redis 세팅
    
    issue: #30

---
+++ src/main/java/com/kestrel/weddingbookkeeper/common/util/RedisConfiguration.java
@@ -0,0 +1,36 @@
+package com.kestrel.weddingbookkeeper.common.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Slf4j
+@Configuration
+public class RedisConfiguration {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        log.info("host: {}, port: {}", host, port);
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
