e92790c5cbb931ed94257a2430d0f5b9e10ddda6
sa46lll <azure394@gmail.com>
2023-07-03 10:21:21 +0900

    feat: 회원 정보 조회 API 구현

--- src/main/java/com/kestrel/weddingbookkeeper/api/member/controller/MemberController.java
+++ src/main/java/com/kestrel/weddingbookkeeper/api/member/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.kestrel.weddingbookkeeper.api.member.controller;
 
 import com.kestrel.weddingbookkeeper.api.auth.utils.WeddingMember;
+import com.kestrel.weddingbookkeeper.api.member.dto.response.MemberInformationResponse;
+import com.kestrel.weddingbookkeeper.api.member.service.MemberService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/members")
 public class MemberController {
 
+    private static final Integer MEMBER_ID = 18;
+
+    private final MemberService memberService;
+
+    public MemberController(final MemberService memberService) {
+        this.memberService = memberService;
+    }
+
     @GetMapping
     public void getMemberTest(@AuthenticationPrincipal WeddingMember weddingMember) {
         Long memberId = weddingMember.getMemberId();
+    }
+
+    @GetMapping("/me")
+    public MemberInformationResponse getMemberInformation(@AuthenticationPrincipal WeddingMember weddingMember) {
+        return memberService.getMemberInformation(MEMBER_ID);
     }
 }
