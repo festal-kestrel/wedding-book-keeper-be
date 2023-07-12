434d10d0b291fbef4eeb99f706c183ca335c4f1c
sa46lll <azure394@gmail.com>
2023-07-12 23:57:03 +0900

    refactor: 인증코드 검증 내부 로직 변경

--- src/main/java/com/kestrel/weddingbookkeeper/api/auth/service/impl/AuthServiceImpl.java
+++ src/main/java/com/kestrel/weddingbookkeeper/api/auth/service/impl/AuthServiceImpl.java
@@ -9,10 +9,8 @@ import com.kestrel.weddingbookkeeper.api.member.vo.Member;
 import com.kestrel.weddingbookkeeper.api.member.vo.Role;
 import com.kestrel.weddingbookkeeper.api.wedding.dao.ManagerVerificationCodeRepository;
 import com.kestrel.weddingbookkeeper.api.wedding.dto.response.ManagerVerificationCodeResponse;
-import com.kestrel.weddingbookkeeper.api.wedding.exception.AlreadyVerifiedException;
 import com.kestrel.weddingbookkeeper.api.wedding.utils.VerificationCodeGenerator;
 import com.kestrel.weddingbookkeeper.api.wedding.vo.ManagerVerificationCode;
-import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -30,25 +28,23 @@ public class AuthServiceImpl implements AuthService {
     @Override
     public VerificationCode getPartnerVerificationCode(final Member member) {
         return verificationCodeRepository.findByMemberId(member.getMemberId())
-                .filter(code -> !code.isVerified())
-                .orElseGet(() -> issuePartnerVerificationCode(member));
+            .filter(code -> !code.isVerified())
+            .orElseGet(() -> issuePartnerVerificationCode(member));
     }
 
     private VerificationCode issuePartnerVerificationCode(final Member member) {
         String verificationCode = VerificationCodeGenerator.generate();
         return verificationCodeRepository.save(
-                VerificationCode.of(verificationCode, Role.PARTNER, member.getMemberId())
+            VerificationCode.of(verificationCode, Role.PARTNER, member.getMemberId())
         );
     }
 
     @Override
     public Long verifyPartnerVerificationCode(VerificationCodeRequest verificationCodeRequest) {
-        VerificationCode verificationCode = verificationCodeRepository.findById(
-                        verificationCodeRequest.getVerificationCode())
+        VerificationCode verificationCode =
+            verificationCodeRepository.findById(verificationCodeRequest.getVerificationCode())
+                .filter(code -> !code.isVerified())
                 .orElseThrow(VerificationCodeNotFoundException::new);
-        if (verificationCode.getRole() != Role.PARTNER || verificationCode.isVerified()) {
-            throw new VerificationCodeNotFoundException();
-        }
         verificationCode.verify();
         verificationCodeRepository.save(verificationCode);
         return verificationCode.getMemberId();
@@ -56,12 +52,10 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public ManagerVerificationCodeResponse verifyManagerVerificationCode(VerificationCodeRequest verificationCodeRequest) {
-        ManagerVerificationCode managerVerificationCode = managerVerificationCodeRepository.findById(
-                        verificationCodeRequest.getVerificationCode())
+        ManagerVerificationCode managerVerificationCode =
+            managerVerificationCodeRepository.findById(verificationCodeRequest.getVerificationCode())
+                .filter(code -> !code.isVerified())
                 .orElseThrow(VerificationCodeNotFoundException::new);
-        if (managerVerificationCode.isVerified()) {
-            throw new AlreadyVerifiedException();
-        }
         managerVerificationCode.verify();
         managerVerificationCodeRepository.save(managerVerificationCode);
         return new ManagerVerificationCodeResponse(managerVerificationCode.getWeddingId());
